b39c3ca3fe65aecb7bda02fbc784fcdec3c84ba7
Anastassia Troitskaia <atroitskaia@gmail.com>
2017-11-13 03:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Code your solutions in this file
 const names =  ['Ada', 'Brenda', 'Ali'];
+
+
 function printBadges (names){
   for (let i = 0; i<names.length; i++) {
     console.log([names]);
   }
-  
 }
 
 
